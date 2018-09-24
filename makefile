999620aa6940fd22311a7c4ebf9b67714915b92e
Ren Wang <iamwrm@gmail.com>
2018-09-24 11:44:47 +0800

    successful saperate the args

--- makefile
+++ makefile
@@ -1,5 +1,6 @@
 CC = clang
-CFLAGS = -std=c11 -O2 -Wall -Wextra -Werror -pedantic -Wno-unused-result
+CFLAGS = -std=c11 -O2 -Wall -Wextra -pedantic  -g
+#CFLAGS = -std=c11 -O2 -Wall -Wextra -Werror -pedantic -Wno-unused-result
 MUMSH_SRC = mumsh.c
 MUMSH = mumsh
 MUMSHMC_FLAGS = -fsanitize=address -fno-omit-frame-pointer -fsanitize=undefined -fsanitize=integer
--- mumsh.c
+++ mumsh.c
@@ -1,13 +1,69 @@
+#define _XOPEN_SOURCE 700
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
+int parse_cmd(char* line, char** argv)
+{
+	int position = 0;
+	char* arg;
+	if (!argv) {
+		fprintf(stderr, "lsh: allocation error\n");
+		exit(EXIT_FAILURE);
+	}
 
-int main(){
+	arg = strtok(line, " \t\r\n\a");
 
+	while (arg != NULL) {
+		argv[position] = arg;
+		position++;
+		arg = strtok(NULL, " \t\r\n\a");
+	}
+	return 0;
+	argv[position] = NULL;
+}
 
+// return if_esc
+int process_cmd(char** argv)
+{
+	// size_t size = strlen(line_input);
+	int size = 1;
+	int i = 0;
 
-
-
+	while (argv[i] != '\0') {
+		printf("%s\n", argv[i]);
+		i++;
+	}
 	return 0;
 }
 
+int main()
+{
+	int bufsize = 1024;
+	char* line = malloc(sizeof(char) * bufsize);
+	// size_t len = 0;
+	size_t capacity = 1024;
+	char argv[64][1024];
+
+	int if_esc = 0;
+
+	char* sh_name = "mumsh $ ";
+	while (1) {
+		printf("%s", sh_name);
+		getline(&line, &capacity, stdin);
+
+		parse_cmd(line, argv);
+
+		if_esc = process_cmd(argv);
+		if (if_esc) {
+			break;
+		}
+	}
+
+	free(line);
+
+	return 0;
+}
