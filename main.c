ed33320fccd5e406c3f05aa4ca9156819c5e2949
Ren Wang <iamwrm@gmail.com>
2018-09-24 17:34:58 +0800

    submitted to oj

---
+++ main.c
@@ -0,0 +1,194 @@
+#define _XOPEN_SOURCE 700
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+void clear_buffer(char* line, char** argv)
+{
+	*line = '\0';
+	int position = 0;
+	while (argv[position] != NULL) {
+		argv[position] = NULL;
+		position++;
+	}
+}
+
+int parse_cmd(char* line, char** argv)
+{
+	int position = 0;
+	char* arg;
+	if (!argv) {
+		fprintf(stderr, "lsh: allocation error\n");
+		exit(EXIT_FAILURE);
+	}
+
+	arg = strtok(line, " \t\r\n\a");
+
+	while (arg != NULL) {
+		argv[position] = arg;
+		position++;
+		arg = strtok(NULL, " \t\r\n\a");
+	}
+	argv[position] = NULL;
+	return 0;
+}
+
+// return if_esc
+int process_cmd(char** argv, char* line)
+{
+	// size_t size = strlen(line_input);
+	int size = 1;
+	int i = 0;
+	if (argv[0] == NULL) {
+		return 0;
+	}
+	if (strcmp(argv[0], "exit") == 0) {
+		free(line);
+		printf("exit\n");
+		fflush(stdout);
+		fflush(stderr);
+		exit(0);
+	}
+
+	// if redirect
+	int file_redirect_flag = 0;
+	int re_l_pos = 0;
+	int re_r_pos = 0;
+
+	i = 0;
+	while (argv[i] != NULL) {
+		if (strcmp(argv[i], ">") == 0) {
+			if (file_redirect_flag == 2) {
+				file_redirect_flag = 4;
+			} else {
+				file_redirect_flag = 1;
+			}
+			re_r_pos = i;
+			argv[i] = NULL;
+			i++;
+			continue;
+		}
+		if (strcmp(argv[i], ">>") == 0) {
+			if (file_redirect_flag == 2) {
+				file_redirect_flag = 5;
+			} else {
+				file_redirect_flag = 3;
+			}
+			re_r_pos = i;
+			argv[i] = NULL;
+			i++;
+			continue;
+		}
+		if (strcmp(argv[i], "<") == 0) {
+			file_redirect_flag = 2;
+			re_l_pos = i;
+			argv[i] = NULL;
+			i++;
+			continue;
+		}
+		i++;
+	}
+	// =================
+
+	pid_t pid;
+	int status;
+
+	pid = fork();
+
+	if (pid == 0) {
+		// child
+		if (file_redirect_flag > 0) {
+			// file redirect happens
+			if (file_redirect_flag == 1) {
+				char* filename = argv[re_r_pos + 1];
+				int outfile =
+				    open(filename, O_CREAT | O_WRONLY, S_IRWXU);
+
+				dup2(outfile, STDOUT_FILENO);
+			}
+			if (file_redirect_flag == 3) {
+				char* filename = argv[re_r_pos + 1];
+				int outfile = open(
+				    filename, O_CREAT | O_APPEND | O_WRONLY,
+				    S_IRWXU);
+
+				dup2(outfile, STDOUT_FILENO);
+			}
+			if (file_redirect_flag == 2) {
+				char* filename = argv[re_l_pos + 1];
+				int outfile = open(filename, O_RDWR | O_CREAT,
+						   S_IRUSR | S_IWUSR);
+
+				dup2(outfile, STDIN_FILENO);
+			}
+			if (file_redirect_flag == 4) {
+				char* out_file_name = argv[re_r_pos + 1];
+				char* in_file_name = argv[re_l_pos + 1];
+				int out_file = open(
+				    out_file_name, O_CREAT | O_WRONLY, S_IRWXU);
+				int in_file = open(in_file_name,
+						   O_CREAT | O_WRONLY, S_IRWXU);
+
+				dup2(out_file, STDOUT_FILENO);
+				dup2(in_file, STDIN_FILENO);
+			}
+			if (file_redirect_flag == 5) {
+				char* out_file_name = argv[re_r_pos + 1];
+				char* in_file_name = argv[re_l_pos + 1];
+				int out_file = open(
+				    out_file_name,
+				    O_CREAT | O_APPEND | O_WRONLY, S_IRWXU);
+				int in_file = open(in_file_name,
+						   O_CREAT | O_WRONLY, S_IRWXU);
+
+				dup2(out_file, STDOUT_FILENO);
+				dup2(in_file, STDIN_FILENO);
+			}
+		}
+
+		if (execvp(*argv, argv) < 0) {
+			printf("system func failed\n");
+			exit(1);
+		}
+
+	} else {  // parent
+		while (wait(&status) != pid) {
+		}
+	}
+	// =================
+	return 0;
+}
+
+int main()
+{
+	int bufsize = 1024;
+	char* line = malloc(sizeof(char) * bufsize);
+	// size_t len = 0;
+	size_t capacity = 1024;
+	char argv[64][1024];
+
+	int if_esc = 0;
+
+	char* sh_name = "mumsh $ ";
+	while (1) {
+		printf("%s", sh_name);
+		getline(&line, &capacity, stdin);
+
+		parse_cmd(line, argv);
+		if_esc = process_cmd(argv, line);
+
+		clear_buffer(line, argv);
+
+		if (if_esc) {
+			break;
+		}
+	}
+
+	free(line);
+
+	return 0;
+}
--- makefile
+++ makefile
@@ -1,25 +1,23 @@
 CC = clang
-CCg = gcc
 CFLAGS = -std=c11 -O2 -Wall -Wextra -pedantic  -g
-CgFLAGS = -std=c11 -O2 -Wall -Wextra -pedantic  -g
 #CFLAGS = -std=c11 -O2 -Wall -Wextra -Werror -pedantic -Wno-unused-result
-MUMSH_SRC = mumsh.c
+MUMSH_SRC = main.c
 MUMSH = mumsh
 MUMSHMC_FLAGS = -fsanitize=address -fno-omit-frame-pointer -fsanitize=undefined -fsanitize=integer
 MUMSHMC = mumsh_memory_check
 .PHONY: clean
 
-#all: $(MUMSH) $(MUMSHMC)
+all: $(MUMSH) $(MUMSHMC)
 	@echo mumsh successfully constructed
 
-all: $(MUMSH_SRC)
-	$(CC) $(CFLAGS) -o $(MUMSH) $(MUMSH_SRC)
+#all: $(MUMSH_SRC)
+	#$(CC) $(CFLAGS) -o $(MUMSH) $(MUMSH_SRC)
 
 $(MUMSH): $(MUMSH_SRC)
 	$(CC) $(CFLAGS) -o $(MUMSH) $(MUMSH_SRC)
 
 $(MUMSHMC) : $(MUMSH_SRC)
-	$(CC) $(CFLAGS) $(MUMSHMC_FLAGS) -o $(MUMSHMC) $(MUMSHMC_SRC)
+	$(CC) $(CFLAGS) $(MUMSHMC_FLAGS) -o $(MUMSHMC) $(MUMSH_SRC)
 
 .c.o:
 	$(CC) $(CFLAGS) -c $< -o $@
@@ -27,8 +25,5 @@ $(MUMSHMC) : $(MUMSH_SRC)
 clean:
 	$(RM) *.o *.a *~ $(MUMSH) $(MUMSHMC)
 
-gcc: $(MUMSH_SRC)
-	$(CCg) $(CgFLAGS) -o $(MUMSH) $(MUMSH_SRC)
-
 tar:all
-	tar -cvzf p1.tar makefile mumsh.c
+	tar -cvzf p1.tar Makefile main.c
